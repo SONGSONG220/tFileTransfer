72b54ac19b3a20096b6e22eb666ba190216163b5
anonymous <anonymous@inke.cn>
2025-11-26 15:13:24 +0800

    [feat] Adjust iosApp

--- appShare/src/iosMain/kotlin/com/tans/tfiletransporter/IosEntry.kt
+++
@@ -1,9 +0,0 @@
-package com.tans.tfiletransporter
-
-import androidx.compose.ui.window.ComposeUIViewController
-import platform.UIKit.UIViewController
-
-class IosEntry {
-    fun rootViewController(): UIViewController = ComposeUIViewController { tFileTransfer() }
-}
-
